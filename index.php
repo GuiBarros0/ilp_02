872aadd2a2920535e2cd75e8e6267f553e7e8fb5
Guilherme Barros <86804512+GuiBarros0@users.noreply.github.com>
2022-09-27 21:32:23 -0300

    Update index.php

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
                 <h1>Desafios</h1>
             </div>
                 <div class="card-item">
-                    <a href="../lab_eng/adivinha_01/adivinha.html" target="_blank"><img class="projeto-img" src="../lab_eng/adivinha_01/img/adivinha.png" alt="desafio Mensagem"></a>
+                    <a href="ilp02/adivinha_01/adivinha.html" target="_blank"><img class="projeto-img" src="../lab_eng/adivinha_01/img/adivinha.png" alt="desafio Mensagem"></a>
                     <div class="card-info">
                         <a class="codigo-github" id="codigo-github" href="https://github.com/jhonatasv/javascript/tree/master/desafios/d001" rel="author" target="_blank">
                             <p>Adivinha</p>
